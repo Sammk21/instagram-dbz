801e703d7d4be0a99ef9202b097daffc2d633338
sameer khan <samkhan@sameers-MacBook-Air.local>
2024-04-06 22:15:02 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,62 +1,45 @@
-// index.js
+// src/app.js
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const socket = require("socket.io");
-const cors = require("cors"); 
+const bodyParser = require("body-parser");
+const courseRoutes = require("./routes/course");
+const cors = require("cors");
 const path = require("path");
-const http = require("http");
-
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-
-// MongoDB connection
-mongoose.connect( process.env.MONGO_URL || "mongodb://localhost:27017/instagram", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-
-// Middleware
-app.use(cors({
- origin: process.env.FRONTEND_URL, 
-  credentials: true,
-  methods: ["GET", "POST"],
-})); 
-
-
-app.use(express.json());
+// Use the cors middleware with options
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// MongoDB connection
+mongoose.connect(
+  process.env.MONGO_URL || "mongodb://localhost:27017/courseDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Middleware
+app.use(bodyParser.json());
 
 // Routes
-app.use("/api/posts", require("./routes/posts"));
+app.use("/courses", courseRoutes);
 
-
-
-
-
-// Start server
-const server = app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
-const io = socket(server,{
-   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.set("io", io);
-
-
-
-
-
-
--- necxis/src/app/page.js
+++ necxis/src/app/page.js
@@ -1,74 +1,29 @@
 "use client";
-import PostCard from "@/modules/PostCard";
+import Admin from "@/modules/admin/AdminDashboard";
 import axios from "axios";
-import Link from "next/link";
 import { useEffect, useState } from "react";
-import io from "socket.io-client";
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
+  const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    const socket = io("http://localhost:9000");
-
-    socket.on("initialPosts", (initialPosts) => {
-      setPosts(initialPosts);
-    });
-
-    socket.on("likeUpdated", ({ postId, likes }) => {
-      setPosts((prevPosts) =>
-        prevPosts.map((post) =>
-          post._id === postId ? { ...post, likes } : post
-        )
-      );
-    });
-    fetchPosts();
-
-    // Clean up socket connection on unmount
-    return () => {
-      socket.disconnect();
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get("http://localhost:9000/courses");
+        setCourses(response.data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
     };
+
+    fetchCourses();
   }, []);
 
-  const fetchPosts = async () => {
-    try {
-      const response = await axios.get("http://localhost:9000/api/posts");
-      setPosts(response.data);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  };
-
-  const handleLikeClick = async (postId) => {
-    console.log("clicked");
-    try {
-      await axios.post(`http://localhost:9000/api/posts/${postId}/like`);
-    } catch (error) {
-      console.error("Error adding like:", error);
-    }
-  };
+  console.log(courses);
 
   return (
     <main className="flex flex-col items-center justify-between p-24 gap-y-6 relative ">
-      <div className="min-h-screen gap-10">
-        {posts.map((post) => (
-          <PostCard handleLike={handleLikeClick} post={post} key={post._id} />
-        ))}
-      </div>
-      <AddPost />
+      <Admin courses={courses} />
     </main>
-  );
-}
-/* eslint-disable jsx-a11y/anchor-is-valid */
-
-function AddPost() {
-  return (
-    <>
-      <Link href={"/addpost"} className=" fixed bottom-0">
-        <div className="w-28 h-28 bg-[#000] rounded-full flex justify-center items-center">
-          add post
-        </div>
-      </Link>
-    </>
   );
 }
